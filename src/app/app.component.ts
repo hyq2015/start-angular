2b71245a872af11e468cdf8efc2ac88a875e1935
ricky <rickyhuang@mfcgd.com>
2019-08-20 17:58:48 +0800

    router, router guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from './auth/auth.service';
 
